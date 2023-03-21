b9f51833874cabc8ace967544f8746f29cc72f1b
Annelise Rocha <annelisesr@gmail.com>
2023-03-21 15:37:42 -0300

    Atividade Quizz semana 3

---
+++ src/semana3e4.py
@@ -0,0 +1,43 @@
+# def maiorValor():
+#   n = int(input("Informe o tamanho da lista: "))
+#   lista = []
+#   for i in range(0, n):
+#     lista.append(input("Digite um valor: "))
+#   return lista
+
+# maiorValor()
+
+# def maiorValor(n):
+#   lista = []
+#   for i in range(0, n):
+#     lista.append(input("Digite um valor: "))
+#   return lista
+
+# maiorValor(5)
+
+def maiorValor(n):
+  lista = []
+  for i in range(0, n):
+    lista.append(input("Digite um valor: "))
+  print(lista)
+
+maiorValor(3)
+
+
+# def maiorValor():
+#   n = int(input("Informe o tamanho da lista: "))
+#   lista = []
+#   for i in range(0, n):
+#     lista.append(input("Digite um valor: "))
+#   print(lista)
+
+#   maiorValor()
+
+
+# def maiorValor(n):
+#   lista = []
+#   for i in lista:
+#     lista.append(input("Digite um valor: "))
+#   print(lista)
+
+#   maiorValor(3)
